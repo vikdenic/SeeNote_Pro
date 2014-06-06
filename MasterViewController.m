b9dbbcba359740d5b33961624b47370fdd5f95a8
vikdenic <vik.denic@gmail.com>
2014-06-05 21:22:26 -0500

    core data established. tableView constructed.

--- MasterViewController.m
+++ MasterViewController.m
@@ -7,26 +7,53 @@
 //
 
 #import "MasterViewController.h"
+#import "CustomTableViewCell.h"
+#import "Picnote.h"
 
-@interface MasterViewController ()
+@interface MasterViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property UITableView *tableView; //NEEDS IBOUTLET
 
 @end
 
 @implementation MasterViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self load];
+}
+
+-(void)load
+{
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"PicNote"];
+    NSSortDescriptor *sortDescriptorByDate = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:NO];
+    request.sortDescriptors = [NSArray arrayWithObjects:sortDescriptorByDate, nil];
+
+    self.fetchedResultsController =[[NSFetchedResultsController alloc]initWithFetchRequest:request managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+
+    [self.tableView reloadData];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.fetchedResultsController.sections[section] numberOfObjects];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Picnote *picNote = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    CustomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    cell.categoryLabel.text = [NSString stringWithFormat:@"%@",[picNote valueForKey:@"category"]];
+    cell.dateLabel.text = [NSString stringWithFormat:@"%@", [picNote valueForKey:@"date"]];
+    cell.cellImageView.image = [UIImage imageWithData:picNote.photo];
+
+    return cell;
 }
 
 
