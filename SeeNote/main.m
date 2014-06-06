9f9bb12cfccf7f5fead702876881e0c6d2746850
vikdenic <vik.denic@gmail.com>
2014-06-05 19:59:50 -0500

    Initial Commit

---
+++ SeeNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SeeNote
+//
+//  Created by Vik Denic on 6/5/14.
+//  Copyright (c) 2014 Vik and Blake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SeeNoteTests/SeeNoteTests.m
@@ -0,0 +1,34 @@
+//
+//  SeeNoteTests.m
+//  SeeNoteTests
+//
+//  Created by Vik Denic on 6/5/14.
+//  Copyright (c) 2014 Vik and Blake. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SeeNoteTests : XCTestCase
+
+@end
+
+@implementation SeeNoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
